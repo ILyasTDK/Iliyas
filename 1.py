c2304ddac04d3d3fe53335a4413575e1f37de137
ILyasTDK <ilyasmuratuly2008@gmail.com>
2026-01-19 17:19:29 +0500

    Add files via upload

---
+++ 1.py
@@ -0,0 +1,88 @@
+print ("hello world")
+
+if 5>2:
+    print("Five is greater than two !")
+print("Python is fun")
+print("Have a good day")
+print("Learning python is fun!")
+print("Hello World!", end=" ")
+print("I will print on the same line.")
+print(3)
+print(358)
+print(50000)
+print(3+3)
+print(2*5)
+print("I am",35,"years old")
+#This is a coment
+#written in
+#more than just one line
+"""
+This is a comment
+written in
+more than just one line
+"""
+x=5
+y="john"
+print(x)
+print(y)
+x=4 #x is of type int
+x="sally" #x is type of str
+print(X)
+X=int(3) #x is 3
+y=str(3) #x is '3'
+z=float(3) # z is 3.0
+x="john"
+#is the same as
+x='John'
+a=4 
+A = "salary"
+# a will not overwrite
+myvar = "John"
+my_var = "John"
+_my_var = "John"
+myVar = "John"
+MYVAR = "John"
+myvar2 = "John"
+myVariablename="john"
+my_variable_name = "john"
+x,y,z="orange","banana","cherry"
+print(x)
+print(y)
+print(z)
+x=y=z="orange"
+print(x)
+print(y)
+print(z)
+fruits = ["banana","apple","cherry"]
+x,y,z=fruits
+print(x)
+print(y)
+print(z)
+x="Python is awesome"
+print(x)
+x="python"
+y="is"
+z="awesome"
+print(x,y,z)
+x="python"
+y="is"
+z="awesome"
+print(x+y+z)
+x=5
+y=10
+print(x+y)
+x="awesome"
+def myfunc ():
+    print("python is"+x)
+myfunc()
+x="awesome"
+def myfunc():
+    print("python is" + x)
+myfunc()
+print("python is" + x)
+def myfunc() :
+    global X
+    x="fantastic"
+
+myfunc()
+print("Python is" + x)
